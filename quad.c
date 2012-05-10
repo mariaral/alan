30159c0393d6266ad88f90dd53d8032e146909c2
maria ralli <mariai.ralli@gmail.com>
2012-05-10 04:41:50 +0300

    Add printQuads().

--- quad.c
+++ quad.c
@@ -8,88 +8,88 @@ quadListNode * quadLast = NULL;
 
 int nextQuad()
 {
-        printf("next quad: %d\n",quadNext);
-        return quadNext;
+    printf("next quad: %d\n",quadNext);
+    return quadNext;
 }
 /*
-operand address_of(operand x)
-{
-    operand r;
+   operand address_of(operand x)
+   {
+   operand r;
 
-    r.opType = OP_PLACE;
-    r.u.place = 
-} 
-*/
+   r.opType = OP_PLACE;
+   r.u.place = 
+   } 
+   */
 void genQuad(oper a, operand b, operand c, operand d)
 {
-        quadListNode* newQuad;
-     
-        newQuad = malloc(sizeof(quadListNode));
-        newQuad->op = a;
-        newQuad->operand0 = b;
-        newQuad->operand1 = c;
-        newQuad->operand2 = d;
-        newQuad->quadLabel = quadNext++;
-        newQuad->next = NULL;
-        if (quadLast!=NULL)
-                quadLast->next = newQuad;
-        else quadFirst = newQuad;
-        quadLast = newQuad;
-/*       printf("%d\n",quadLast->id);*/
+    quadListNode* newQuad;
+
+    newQuad = malloc(sizeof(quadListNode));
+    newQuad->op = a;
+    newQuad->operand0 = b;
+    newQuad->operand1 = c;
+    newQuad->operand2 = d;
+    newQuad->quadLabel = quadNext++;
+    newQuad->next = NULL;
+    if (quadLast!=NULL)
+        quadLast->next = newQuad;
+    else quadFirst = newQuad;
+    quadLast = newQuad;
+    /*       printf("%d\n",quadLast->id);*/
 }
 
 SymbolEntry * newTemp(Type type)
 {
-        return (newTemporary(type));
+    return (newTemporary(type));
 }
 
 labelList * emptyList()
 {
-        labelList *p;
+    labelList *p;
 
-        p = malloc(sizeof(labelList));
-        p->label = NULL;
-        p->next = NULL;
-        return p;
+    p = malloc(sizeof(labelList));
+    p->label = NULL;
+    p->next = NULL;
+    return p;
 }
 
 labelList * makeList(int x)
 {
-        quadListNode *temp;
-        labelList *p;
-        
-        p = malloc(sizeof(labelList));
-        temp = quadFirst;
-        while((temp!=NULL)&&(temp->quadLabel!=x))
-                temp = temp->next;
-        p->label = temp;
-        p->next = NULL;
-        return p;
+    quadListNode *temp;
+    labelList *p;
+
+    p = malloc(sizeof(labelList));
+    temp = quadFirst;
+    while((temp!=NULL)&&(temp->quadLabel!=x))
+        temp = temp->next;
+    p->label = temp;
+    p->next = NULL;
+    return p;
 }
 
 labelList * merge(labelList * l1, labelList * l2)
 {
-        labelList *temp;
-        temp = l1;
-        while(temp->next!= NULL)
-                temp = temp->next;
-        temp->next = l2;
-        return l1;
+    labelList *temp;
+    temp = l1;
+    while(temp->next!= NULL)
+        temp = temp->next;
+    temp->next = l2;
+    return l1;
 }
 
 void backpatch(labelList * l, int z)
 {
-        labelList * temp;
+    labelList * temp;
 
-        temp = l;
-        while(temp!=NULL) {
+    temp = l;
+    while(temp!=NULL) {
 
-                if (temp->label->operand2.opType==OP_UNKNOWN) {
-                        temp->label->operand2.opType = OP_LABEL;
-                        temp->label->operand2.u.label = z;
-                }
-                temp = temp->next;
+        if (temp->label->operand2.opType==OP_UNKNOWN) {
+            temp->label->operand2.opType = OP_LABEL;
+            temp->label->operand2.u.label = z;
         }
+        temp = temp->next;
+    }
 }
 
 void printQuads()
@@ -97,4 +97,79 @@ void printQuads()
     quadListNode * temp;
 
     temp = quadFirst;
+    while (temp != NULL) {
+        printf("%d: ",temp->quadLabel);
+        switch (temp->op) {
+        case UNIT:
+            printf("unit, ");
+            break;
+        case ENDU:
+            printf("endu, ");
+            break;
+        case PLUS:
+            printf("+, ");
+            break;
+        case MINUS:
+            printf("-, ");
+            break;
+        case MULT:
+            printf("*, ");
+            break;
+        case DIVI:
+            printf("/, ");
+            break;
+        case MOD:
+            printf("%%, ");
+            break;
+        case ASSIGN:
+            printf(":=, ");
+            break;
+        case ARRAY:
+            printf("array, ");
+            break;
+        }
+        printOp(temp->operand0);
+        printf(", ");
+        printOp(temp->operand1);
+        printf(", ");
+        printOp(temp->operand2);
+        printf("\n");
+        temp = temp->next;
+    }
 }
+
+void printOp(operand op)
+{
+    Place p;
+
+    switch (op.opType) {
+    case OP_LABEL:
+        printf("%d",op.u.label);
+        break;
+    case OP_NAME:
+        printf("%s",op.u.name);
+        break;
+    case OP_UNKNOWN:
+        printf("*");
+        break;
+    case OP_NOTHING:
+        printf("-");
+        break;
+    case OP_PLACE:
+        p=op.u.place;
+        switch (p.placeType) {
+        case CONSTNUM:
+            printf("%d",p.u.constnum);
+            break;
+        case CONSTCHAR:
+            printf("'%c'",p.u.constchar);
+            break;
+        case STRING:
+            printf("%s",p.u.string);
+            break;
+        }
+    }
+}
+
+
+
